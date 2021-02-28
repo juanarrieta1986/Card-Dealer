3cfc1d9cda8c7bb378d4dac7d2f66559bcc89ce3
juanarrieta1986 <juanca86@gmail.com>
2021-02-28 03:11:43 +0000

    v3

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let arrayPalos = ["♦","♥","♠","♣"];
 let arrayCartas = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+let testArray = [1,2,3,4];
 let palo, carta;
+//console.log(testArray.length);
+
 
 function dealCard (){
-    palo = Math.floor(Math.random() * (arrayPalos.lenght));
-    console.log(palo);
-    carta = Math.floor(Math.random() * (arrayCartas.lenght));
-    console.log(carta);
+    palo = Math.floor(Math.random()*(arrayPalos.length));
+    carta = Math.floor(Math.random() * (arrayCartas.length));
+    //console.log(arrayCartas.length);
 }
 
 window.onload = function() {
-    dealCard;
-    document.getElementById("palo1").innerHTML = palo;
-    this.document.getElementById("palo2").innerHTML = palo;
-    this.document.getElementById("numero").innerHTML = carta;
+    dealCard();
+    document.getElementById("palo1").innerHTML = arrayPalos[palo];
+    this.document.getElementById("palo2").innerHTML = arrayPalos[palo];
+    this.document.getElementById("numero").innerHTML = arrayCartas[carta];
 
 };
